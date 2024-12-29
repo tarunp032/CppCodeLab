58880ca7c7df83ab5965afda45acdf41db59fc1a
Tarun Prajapat <140405189+tarunp032@users.noreply.github.com>
2024-12-29 18:02:10 +0530

    Update HollowTrianglePattern.cpp

--- HollowTrianglePattern.cpp
+++ HollowTrianglePattern.cpp
@@ -6,17 +6,14 @@ int main() {
     cout << "Enter the number of rows: ";
     cin >> rows;
 
-    // Loop through each row
     for (int i = 1; i <= rows; i++) {
-        // Loop through each column in a row
         for (int j = 1; j <= i; j++) {
-            // Print '*' for the first and last columns, and the last row
             if (j == 1 || j == i || i == rows)
                 cout << "*";
             else
-                cout << " ";  // Print space for the hollow part
+                cout << " ";  
         }
-        cout << endl;  // Move to the next line after each row
+        cout << endl;  
     }
     
     return 0;
