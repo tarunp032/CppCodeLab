0e8e0b23dc29e1937dc53aed103bc349997db4f7
Tarun Prajapat <140405189+tarunp032@users.noreply.github.com>
2024-12-29 18:00:29 +0530

    Update Armstrong.cpp

--- Armstrong.cpp
+++ Armstrong.cpp
@@ -11,7 +11,6 @@ int main() {
 
     originalNum = num;
 
-    // Count the number of digits
     while (originalNum != 0) {
         originalNum /= 10;
         ++n;
@@ -19,14 +18,12 @@ int main() {
 
     originalNum = num;
 
-    // Calculate the sum of powers of digits
     while (originalNum != 0) {
         remainder = originalNum % 10;
         result += pow(remainder, n);
         originalNum /= 10;
     }
 
-    // Check if the number is an Armstrong number
     if (result == num)
         cout << num << " is an Armstrong number.";
     else
